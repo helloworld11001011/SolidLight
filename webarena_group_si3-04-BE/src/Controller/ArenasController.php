86b5b77bec4de2aca377dd35b4b46bb651b6a762
axelcante <axel.e.cante@gmail.com>
2017-10-17 09:14:17 +0200

    axel/small formating and starting new work

--- webarena_group_si3-04-BE/src/Controller/ArenasController.php
+++ webarena_group_si3-04-BE/src/Controller/ArenasController.php
@@ -10,59 +10,56 @@ class ArenasController  extends AppController
 {
     public function index()
     {
-        //die('test');
         $this -> loadModel('Fighters');
         $this->set('myname', "Eliott Segard");
         $this -> set('bestFighter', $this -> Fighters -> getBestFighter());
-        
     }
-    
+
     public function login()
     {
-        
+
     }
-    
+
     public function fighter()
     {
         $this -> loadModel('Fighters');
         $figterlist = $this->Fighters->find('all');
         //pr($figterlist -> toArray());
-        
+
         //Pass info to the view
         $this -> set('bestFighter', $this -> Fighters -> getBestFighter());
-        
     }
-    
+
     public function sight()
     {
-        
+
     }
-    
+
     public function diary()
     {
-        
+
     }
-   
-   /* 
+
+   /*
     * Cours du prof pour les formulaires, verifie que les info envoyez son bien en POST pour ensuite les traiter
-    * 
+    *
     public function profile()
     {
-        
+
         $this->loadModel("Player");
-        
+
          if($this->request->is("post")) {
-            
+
             $this->request->getData("email");
             ...
-                          
+
         }
-        
+
         $player = $this->gett(42);
         $this->set("player", $player);
-        
-       
+
+
     }
     */
-    
+
 }
