c35dc1f91603e1dcb6a64e6cb3e7a54f682bf544
axelcante <axel.e.cante@gmail.com>
2017-10-25 17:02:31 +0200

    axel/ second jqplot coming along reeeeeeally slowly

--- webarena_group_si3-04-BE/src/Model/Table/EventsTable.php
+++ webarena_group_si3-04-BE/src/Model/Table/EventsTable.php
@@ -6,15 +6,20 @@ use Cake\ORM\Table;
 use Cake\ORM\TableRegistry;
 
 class EventsTable extends Table {
-    
+
     //Gets the total amount of dead fighters
     //TODO: get them with a date in order to build timeline of deaths in hall of fame
     function getDeadFighters () {
-        $deadFighterQuery = $this->find('all', array(
-            'conditions' => array('Events.name LIKE "Death of %"')
-        ));
-        $deadFighterCount = $deadFighterQuery->count();
-        pr($deadFighterCount);
+        $Query = $this->find('all');
+        $Query
+            ->select([
+                'Events.date',
+                'count' => $Query->func()->count('*')
+            ])
+            ->where(['Events.name LIKE "Death %"'])
+            ->group('MONTH(Events.date)');
+        $Array = $Query->toArray();
+        return $Array;
     }
 }
 
