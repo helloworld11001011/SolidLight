b044d0484a63f85b01dc1d72581a6f07385d4aa6
Baptiste <duplanbaptiste@gmail.com>
2017-10-25 19:22:44 +0200

    addEvent amélioré // pas finit

--- webarena_group_si3-04-BE/src/Model/Table/EventsTable.php
+++ webarena_group_si3-04-BE/src/Model/Table/EventsTable.php
@@ -10,35 +10,54 @@ class EventsTable extends Table {
 
     //Gets the total amount of dead fighters
     //TODO: get them with a date in order to build timeline of deaths in hall of fame
-    function getDeadFighters () {
+    function getDeadFighters() {
         $Query = $this->find('all');
         $Query
-            ->select([
-                'Events.date',
-                'count' => $Query->func()->count('*')
-            ])
-            ->where(['Events.name LIKE "Death %"'])
-            ->group('MONTH(Events.date)');
+                ->select([
+                    'Events.date',
+                    'count' => $Query->func()->count('*')
+                ])
+                ->where(['Events.name LIKE "Death %"'])
+                ->group('MONTH(Events.date)');
         $Array = $Query->toArray();
         return $Array;
     }
-    
-     function addNewEvent() {
+
+    function addNewEvent($arg) {
 
         $eventTable = TableRegistry::get('events');
         $event = $eventTable->newEntity();
         
-        //ajoute un truc au champs 'name' selon le data reçu
-        $event->name = "Bobby attaque Paul et le touche";
-        $event->date = Time::now();
-        $event->coordinate_x = 1;
-        $event->coordinate_y = 1;
-        
+        echo "ok";
+
+        if ($arg == 1) {
+
+            $event->name = "Bobby attaque Paul et le tue";
+            $event->date = Time::now();
+            $event->coordinate_x = 1;
+            $event->coordinate_y = 1;
+            
+        } else if ($arg == 2) {
+
+            $event->name = "Bobby attaque Paul et le touche";
+            $event->date = Time::now();
+            $event->coordinate_x = 1;
+            $event->coordinate_y = 1;
+            
+        } else if ($arg == 3) {
+
+            $event = "Bobby attaque Paul et le rate";
+            $event->date = Time::now();
+            $event->coordinate_x = 1;
+            $event->coordinate_y = 1;
+        }
+
+        echo "<br>";
         echo " evenement: Bobby attaque Paul et le touche --> créé";
 
         $eventTable->save($event);
     }
+
 }
 
-
- ?>
+?>
