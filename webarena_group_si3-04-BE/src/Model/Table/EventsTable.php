a27fab89cfdcde219dedc2a57c6dfe04f9573959
axelcante <axel.e.cante@gmail.com>
2017-10-26 12:02:39 +0200

    axel/ retrieving months from DB

--- webarena_group_si3-04-BE/src/Model/Table/EventsTable.php
+++ webarena_group_si3-04-BE/src/Model/Table/EventsTable.php
@@ -11,21 +11,26 @@ class EventsTable extends Table {
     //Gets the total amount of dead fighters
     //TODO: get them with a date in order to build timeline of deaths in hall of fame
     function getDeadFighters() {
-        $Query = $this->find('all');
-        $Query
-                ->select([
-                    'Events.date',
+        $Query = $this->find();
+        $month = $Query->func()->month([
+            'date' => 'identifier'
+        ]);
+        // pr($month);
+        $Query->select([
+                    'month' => $month,
                     'count' => $Query->func()->count('*')
                 ])
-                ->where(['Events.name LIKE "Death %"'])
+                ->where(['name LIKE "Death %"'])
                 ->group('MONTH(Events.date)');
+
         $Array = $Query->toArray();
 
         //Converting the CakePHP Object Array into a regular array
         for ($i=0; $i < $Query->count(); $i++) {
-            $deadFightersArray[$i][0] = $Array[$i]->date;
+            $deadFightersArray[$i][0] = $Array[$i]->month;
             $deadFightersArray[$i][1] = $Array[$i]->count;
         }
+        pr($deadFightersArray);
         return $deadFightersArray;
     }
 
